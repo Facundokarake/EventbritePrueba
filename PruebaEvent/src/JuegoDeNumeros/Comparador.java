374a5c32e2418ba9c5260a543915c3a5af8ee2f3
SISTEMAS <SISTEMAS@Shainy>
2018-05-07 15:19:59 -0300

    Terminada la primera parte

--- PruebaEvent/src/JuegoDeNumeros/Comparador.java
+++ PruebaEvent/src/JuegoDeNumeros/Comparador.java
@@ -5,10 +5,27 @@
  */
 package JuegoDeNumeros;
 
+import JuegoDeNumeros.Pantallas.*;
+
+
 /**
  *
  * @author SISTEMAS
  */
 public class Comparador {
     
+    
+  
+    Felicitaciones feli = new Felicitaciones();
+    
+    public String Comparar (int hum){
+           Main.Intentos = Main.Intentos+1;
+         System.out.println("comparando");
+    if  (Main.numComputadora>hum)return"mmm intenta con uno mas grande";
+     else if (Main.numComputadora<hum) return "te pasaste machote";
+               
+        return "Ganaste fiera,tifon,torbellino,maquina,crack,numero 1";
+              
 }
+
+}    
--- PruebaEvent/src/JuegoDeNumeros/Main.java
+++ PruebaEvent/src/JuegoDeNumeros/Main.java
@@ -5,17 +5,30 @@
  */
 package JuegoDeNumeros;
 
+import JuegoDeNumeros.Pantallas.Juego;
+
 /**
  *
  * @author SISTEMAS
  */
 public class Main {
 
+    public static int AcumuladorError=0;
+    public static int Intentos=0;
+public static int numComputadora = (int) (Math.random() * 100 + 1); 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+Juego fa = new Juego(); 
+fa.setVisible(true); 
+            System.out.println(fa);
+      
+            
     }
+
+  
     
 }
