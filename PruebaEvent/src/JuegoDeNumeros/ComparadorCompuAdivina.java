cd893e489a6608344f86e7394e77f49fd9da9bda
DESKTOP-A50VPMJ\facun <facundokarake@gmail.com>
2018-05-12 18:02:07 -0300

    Terminado con comentarios

--- PruebaEvent/src/JuegoDeNumeros/ComparadorCompuAdivina.java
+++ PruebaEvent/src/JuegoDeNumeros/ComparadorCompuAdivina.java
@@ -12,12 +12,12 @@ import JuegoDeNumeros.Pantallas.Compuadivina;
  * @author SISTEMAS
  */
 public class ComparadorCompuAdivina {
-
+//declaro las variables
     private int varMax = 100;
     private int varMenor = 0;
     private int IntentosCompu = 0;
     private int numCompu = (int) (Math.random() * 100 + 1);
-
+//creo los gets y sets de las variables privadas
     public int getIntentosCompu() {
         return IntentosCompu;
     }
@@ -33,26 +33,28 @@ public class ComparadorCompuAdivina {
     public void setNumCompu(int numCompu) {
         this.numCompu = numCompu;
     }
-
+//metodo que analiza cuando el numero a adivinar es menor al que mostro la compu
     public void menos() {
-
+//analizo el limite
         if (varMax >= numCompu) {
             varMax = numCompu;
         }
-
+// busco un numero aletorio entre los limites establecidos 
         numCompu = (int) (Math.random() * (varMax - varMenor) + (varMenor));
         Compuadivina.txtCompu.append("asi que mas chico entonces es " + numCompu + "\n");
+                //subo 1 el acumulador de intentos de la compu
         IntentosCompu = IntentosCompu + 1;
     }
-
+//metodo que analiza cuando el numero a adivinar es mayor al que mostro la compu
     public void mas() {
-
+//Actualizo el limite menor 
         if (varMenor <= numCompu) {
             varMenor = numCompu;
         }
-
+// calculo un numero aletorio entre los limites establecidos
         numCompu = (int) (Math.random() * (varMax - varMenor) + (varMenor));
         Compuadivina.txtCompu.append("asi que mas grande entonces es " + numCompu + "\n");
+        //subo 1 el acumulador de intentos de la compu
         IntentosCompu = IntentosCompu + 1;
     }
 
